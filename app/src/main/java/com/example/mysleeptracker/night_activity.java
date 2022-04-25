15d83a9d0b7c4e4b00fb7e493630db801dfb4df6
tahasafdari <41860385+tahasafdari@users.noreply.github.com>
2022-04-25 18:25:07 +0300

    Initial commit
    First commit. UI ready!

---
+++ app/src/main/java/com/example/mysleeptracker/night_activity.java
@@ -0,0 +1,14 @@
+package com.example.mysleeptracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class night_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night);
+    }
+}
